cdad897c28d53d2f9e4dcffd87d5048fab25127d
Sadhana S <tausd05@gmail.com>
2026-08-13 20:41:39 +0530

    updated profile, added config

---
+++ config.js
@@ -0,0 +1,15 @@
+// Local Configuration - DO NOT COMMIT THIS FILE TO GITHUB
+// This file is listed in .gitignore so your private social links stay safe.
+
+const CONFIG = {
+  // Profile Picture Path (e.g., "profile.png" or "AWS Student Builder Group_RGB_Program Icon_Purple.png")
+  profileImage: "AWS Student Builder Group_RGB_Program Icon_Purple.svg",
+  profileAlt: "AWS SBG DBIT Logo",
+
+  // Social Links & Contact Details
+  whatsapp: "https://whatsapp.com/channel/0029Vb76rEYATRSlFR1mOg2X",
+  instagram: "https://www.instagram.com/awssbg_dbit",
+  linkedin: "https://www.linkedin.com/company/aws-student-builders-group-at-don-bosco-institute-of-technology/",
+  meetup: "https://www.meetup.com/aws-sbg-at-don-bosco-institute-of-technology-bangalore",
+  email: "aws.sbg@dbit.co.in"
+};
